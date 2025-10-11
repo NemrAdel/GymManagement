22ef436f5f0343ddc55938e669e1662fb1f9759b
Tiger <nemradel220@yahoo.com>
2025-10-11 21:36:11 +0300

    save

---
+++ GymManagementDAL/Repositories/Implemintation/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using GymManagementDAL.Repositories.Interfaces;
+using GymManagmentDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Implemintation
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity, new()
+    {
+        public int Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GymManagementDAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using GymManagmentDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    internal interface IGenericRepository<T>where T: BaseEntity, new()
+    {
+        T? GetById(int id);
+
+        IEnumerable<T> GetAll(int id);
+
+        int Add(T entity);
+        int Update(T entity);
+        T? Remove(T entity);
+
+    }
+}
