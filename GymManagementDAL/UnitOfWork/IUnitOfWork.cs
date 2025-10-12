950b0410a831f0f97c6427a2e91578d0227f82cc
Tiger <nemradel220@yahoo.com>
2025-10-12 23:05:54 +0300

    save

---
+++ GymManagementDAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using GymManagementDAL.Repositories.Interfaces;
+using GymManagmentDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T> GetRepository<T>()where T : BaseEntity, new();
+
+        int saveChanges();
+    }
+}
---
+++ GymManagementDAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using GymManagementDAL.Repositories.Implemintation;
+using GymManagementDAL.Repositories.Interfaces;
+using GymManagmentDAL.Data.Context;
+using GymManagmentDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.UnitOfWork
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly Dictionary<Type, object> _repositories=new ();
+        private readonly GymDbContext _dbContext;
+
+        public UnitOfWork(GymDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IGenericRepository<T> GetRepository<T>() where T : BaseEntity, new()
+        {
+            // T ==Member
+            var entityType = typeof(T); // Member
+            if (_repositories.ContainsKey(entityType))
+            {
+                return (IGenericRepository<T>)_repositories[entityType];
+            }
+
+            if (_repositories.TryGetValue(entityType, out var repository))
+                return (IGenericRepository<T>)repository;
+
+            var newRepository = 
+                new GenericRepository<T>(_dbContext);
+            _repositories[entityType] = newRepository;
+
+            return newRepository;
+        }
+
+        public int saveChanges()
+        {
+            return _dbContext.SaveChanges();
+        }
+    }
+}
